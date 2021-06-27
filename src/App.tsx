08ef8e10785e54407365f84b9cef9dc365f871a2
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2021-06-27 22:09:21 +0200

    typescript and eslint configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Portfolio from './Components/Portfolio/Portfolio'
+
+function App() {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Portfolio/Portfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    <div>
+      Emir Haktan Öztürk
+    </div>
+  )
+}
+
+export default Portfolio
