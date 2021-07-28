b38eee28d4fcb42addbd9d3183b6ffb657d73629
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2021-07-28 23:53:54 +0200

    update portfolio based on new components

--- src/Components/portfolio/portfolio.tsx
+++ src/Components/portfolio/portfolio.tsx
@@ -1,5 +1,8 @@
 import React, { useRef, useEffect } from 'react'
 import { useInViewport } from 'react-in-viewport'
+import Bubble from './bubble'
+import TechIUse from './tech-i-use'
+import Typing from 'react-typing-animation'
 import { store } from '../../store/createStore'
 import displayReducerActionCreator from '../../store/reducers/display-reducer/display-reducer-action-creator'
 import { ComponentsInViewport } from '../../store/reducers/display-reducer/display-reducer-types'
@@ -23,8 +26,29 @@ function Portfolio(props: any) {
   })
 
   return (
-    <div className="portfolio" id='portfolio' ref={viewPortRef}>
-
+    <div className="portfolio" id="portfolio" ref={viewPortRef}>
+      <Bubble >
+        {
+          inViewport &&
+          (
+            <>
+              <span className="self-description">
+                <span>
+                  <Typing speed={10}>
+                    <p>I am a software engineer living in Warsaw, Poland. Currently i work for Microstrategy Poland.</p>
+                    <Typing.Delay ms={500} />
+                    <p>I love programming, travelling, doing sport and playing computer games...</p>
+                    <Typing.Delay ms={500} />
+                  </Typing>
+                </span>
+              </span>
+              <span className="tech-i-use">
+                <TechIUse />
+              </span>
+            </>
+          )
+        }
+      </Bubble>
     </div>
   )
 }
